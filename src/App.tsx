7cda8920413e87a8f6076bb0b2e8e333c248e97e
mohammadrahman <obaidurapu12@gmail.com>
2025-03-25 02:59:18 +0200

    lint updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="this should push it to the next line and format the codes but it doesnot"
+      />
+    </>
+  );
 };
 
 export default App;
